fd41ae42cb78f0278778f9b11152be41a8e1bc62
prasantakumarsethi <prasanta2001.official@gmail.com>
2024-08-28 00:32:08 +0530

    Added my protfolio project

---
+++ index.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <meta name="author" content="Prasanta Kumar Sethi" />
+
+    <link rel="icon" type="image/x-icon" href="./assets/images/profile_img.png" />
+
+    <meta property="og:title" content="Keshav Kumar" />
+
+    <meta property="og:site_name" content="Keshav Kumar's Portfolio" />
+
+    <meta property="og:url" content="keshavop" />
+
+    <meta property="og:description" content="Software Engineer " />
+
+    <meta property="og:type" content="website" />
+
+    <meta property="og:image" content="" />
+
+    <meta name="title" content="Prasanta Kumar's Portfolio" />
+
+    <meta name="description" content="Software Engineer " />
+
+    <meta name="keywords" content="Web Developer " />
+
+    <meta property="twitter:card" content="summary_large_image" />
+
+    <meta property="twitter:url" content="keshavop" />
+
+    <meta property="twitter:title" content="Keshav Kumar's Portfolio" />
+
+    <meta property="twitter:description" content="Software Engineer " />
+
+    <meta property="twitter:image" content="" />
+
+    <title>Prasanta Kumar's Portfolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <!-- Font Awesome icons (free version)-->
+
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+
+    <!-- Google fonts-->
+
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+
+    <!-- Core theme CSS (includes Bootstrap)-->
+
+    <link href="https://startbootstrap.github.io/startbootstrap-resume/css/styles.css" rel="stylesheet" />
+
+</head>
+
+<body id="page-top">
+
+    <!-- Navigation-->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+
+            <span class="d-block d-lg-none">Prasanta Kumar Sethi</span>
+
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="./assets/images/profile_img.png" alt="Prasanta Kumar Sethi" /></span>
+
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+
+            </ul>
+
+        </div>
+
+    </nav>
+
+    <!-- Page Content-->
+
+    <div class="container-fluid p-0">
+
+        <!-- About-->
+
+        <section class="resume-section" id="about">
+
+            <div class="resume-section-content">
+
+                <h1 class="mb-0">
+
+                    Prasanta
+
+                    <span class="text-primary">Kumar</span>
+                    <span class="text-secondary">Sethi</span>
+
+
+                </h1>
+
+                <div class="subheading mb-5">
+
+                    Odisha &bull; 8249931194 &bull;
+
+                    <a href="mailto:keshavkumar8122@gmail.com">prasanta2001.official@gmail.com</a>
+
+                </div>
+
+                <p class="lead mb-5">Software Engineer </p>
+
+                <div class="social-icons">
+
+                    <a class="social-icon" href="https://www.facebook.com/prasantakumar.sethi.35/"><i class="fab fa-facebook-f"></i></a>
+
+                    <a class="social-icon" href="https://wa.me/8249931194"><i class="fab fa-whatsapp"></i></a>
+
+                    <a class="social-icon" href="https://www.linkedin.com/in/prasanta-kumar-sethi-748578181/"><i class="fab fa-linkedin-in"></i></a>
+
+                    <a class="social-icon" href="https://github.com/prasantakumarsethi"><i class="fab fa-github"></i></a>
+
+                    <a class="social-icon" href="https://stackoverflow.com/users/24109296/prasanta-kumar-sethi"><i class="fab fa-stack-overflow"></i></a>
+
+                </div>
+
+                <div class=" text-success mt-5">
+                    <h3>2 years of work experience as Full-stack Java Developer with experience in all aspects of software development life cycle including Requirement Analysis and Implementation in Scalable Web Applications.</h3>
+                </div>
+            </div>
+
+        </section>
+
+        <hr class="m-0" />
+
+        <!-- Experience-->
+        <section class="resume-section" id="experience">
+
+            <div class="resume-section-content">
+
+                <h2 class="mb-5">Experience</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+
+                    <div class="flex-grow-1">
+
+                        <h3 class="mb-0">Csm Technologies Pvt. Ltd. </h3>
+
+                        <div class="subheading mb-3">Software Engineer</div>
+
+                        <!-- <p>GPA: 8.2</p> -->
+
+                    </div>
+
+                    <div class="flex-shrink-0"><span class="text-primary">August 2022 - Till now</span></div>
+
+                </div>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+
+                    <div class="flex-grow-1">
+
+                        <h3 class="mb-0">MS Tech Mahindra </h3>
+
+                        <div class="subheading mb-3">Associate</div>
+
+                        <!-- <p>GPA: 8.2</p> -->
+
+                    </div>
+
+                    <div class="flex-shrink-0"><span class="text-primary">May 2022 -Aug 2022</span></div>
+
+                </div>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+
+                    <div class="flex-grow-1">
+
+                        <h3 class="mb-0">Nile technologies Pvt. Ltd.</h3>
+
+                        <div class="subheading mb-3">Trainee Software Developer</div>
+
+                        <!-- <p>GPA: 8.2</p> -->
+
+                    </div>
+
+                    <div class="flex-shrink-0"><span class="text-primary">November 2021 -May 2022</span></div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <hr class="m-0" />
+
+        <!-- Education-->
+
+        <section class="resume-section" id="education">
+
+            <div class="resume-section-content">
+
+                <h2 class="mb-5">Education</h2>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+
+                    <div class="flex-grow-1">
+
+                        <h3 class="mb-0">Nalanda Institute Of technology</h3>
+
+                        <div class="subheading mb-3">CSE</div>
+
+                        <p>CGPA: 8.48</p>
+
+                    </div>
+
+                    <div class="flex-shrink-0"><span class="text-primary">Aug 2018 - Jul 2022</span></div>
+
+                </div>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+
+                    <div class="flex-grow-1">
+
+                        <h3 class="mb-0">Higher Seconday College</h3>
+
+                        <div class="subheading mb-3">Science</div>
+
+                        <p>PERCENTAGE: 53.50</p>
+
+                    </div>
+
+                    <div class="flex-shrink-0"><span class="text-primary">Apr 2016 - April 2018</span></div>
+
+                </div>
+
+
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+
+                    <div class="flex-grow-1">
+
+                        <h3 class="mb-0">Regional High School</h3>
+
+                        <div class="subheading mb-3">General Studies</div>
+
+                        <p>PERCENTAGE: 81.00</p>
+
+                    </div>
+
+                    <div class="flex-shrink-0"><span class="text-primary">April 2007 - Mar 2016</span></div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <hr class="m-0" />
+
+        <!-- Skills-->
+
+        <section class="resume-section" id="skills">
+
+            <div class="resume-section-content">
+
+                <h2 class="mb-5">Skills</h2>
+
+                <div class="subheading mb-3">Programming Languages & Tools</div>
+
+                <ul class="fa-ul mb-0">
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        C
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Core Java
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Advanced Java
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Spring Boot
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Hibernate
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        HTML 5
+
+                    </li>
+
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        CSS 3
+
+                    </li>
+
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Javascript ES 6
+
+                    </li>
+
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Bootstrap 5.x
+
+                    </li>
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        Angular 14
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        MySql
+
+                    </li>
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+
+                        PostgreSql
+
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </section>
+
+        <hr class="m-0" />
+
+        <!-- Interests-->
+
+        <section class="resume-section" id="interests">
+
+            <div class="resume-section-content">
+
+                <h2 class="mb-5">Interests</h2>
+
+                <p>Web Development </p>
+
+                <p>hackathon </p>
+
+                <p>Tech Events </p>
+
+                <p>Travelling </p>
+
+            </div>
+
+        </section>
+
+        <hr class="m-0" />
+
+        <!-- Awards-->
+
+        <section class="resume-section" id="awards">
+
+            <div class="resume-section-content">
+
+                <h2 class="mb-5">Awards & Certifications</h2>
+
+                <ul class="fa-ul mb-0">
+
+                    <li>
+
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+
+                        CSM Maverick Award
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+
+        </section>
+
+    </div>
+
+    <!-- Bootstrap core JS-->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Core theme JS-->
+
+    <script src="https://startbootstrap.github.io/startbootstrap-resume/js/scripts.js"></script>
+
+</body>
+
+</html>
